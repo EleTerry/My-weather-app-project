cc504ddcb37def3303a1adb6961243ef1df1913c
EleTerry <68866391+EleTerry@users.noreply.github.com>
2020-09-11 20:54:33 +0100

    Injected a part of HTML in JS

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 function formatDate(timestamp) {
   let date = new Date(timestamp);
+  return `${formatHours(timestamp)}`;
+}
+
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
   let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
@@ -17,7 +22,11 @@ function search(city) {
   let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
   let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(showTemperature);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(showForecast);
 }
+
 function handleSubmit(event) {
   event.preventDefault();
   let city = document.querySelector("#text-input").value;
@@ -50,6 +59,32 @@ function showTemperature(response) {
   document.querySelector(".time").innerHTML = formatDate(
     response.data.dt * 1000
   );
+}
+
+function showForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+
+  for (let index = 0; index < 6; index++) {
+    let forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+  <div class="col-2">
+              <h4>
+              ${formatHours(forecast.dt * 1000)}
+              </h4>
+              <img src ="http://openweathermap.org/img/wn/${
+                forecast.weather[0].icon
+              }@2x.png" id="forecast-icon" /> 
+              <div class="forecast-text">
+                   ${Math.round(forecast.main.temp_min)}°/ 
+                   <strong>
+                   ${Math.round(forecast.main.temp_max)}°
+                   </strong>
+             </div>
+        </div>
+        `;
+  }
 }
 
 function showFahrenheitTemperature(event) {
